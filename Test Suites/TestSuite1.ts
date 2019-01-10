b74d75db3bec6e2f270eaa673a571d1a35fced78
Nareshreddy22 <zeeltester123@gmail.com>
2019-01-10 11:35:07 +0530

    added time

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2019-01-10T11:32:41</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>72180d5c-2345-4d84-a8e7-281320b580b1</testSuiteGuid>
+   <testCaseLink>
+      <guid>77f83eb5-f7c0-4ba8-a884-088705d5d7d3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CIS_Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b705f699-2e02-4246-b0e9-e71c9e7cc91a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
